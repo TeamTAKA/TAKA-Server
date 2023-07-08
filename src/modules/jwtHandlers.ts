1bb1b7016183df53fcdd4c1a4bfc91844a0877a2
Say-young <seyo_oung@ewhain.net>
2023-07-08 13:43:08 +0900

    [add] token 재발급 과정 추가
    * 이미 존재하는 아이디 에러코드 400으로 변경
    * 소셜 로그인 api 추가
    * refreshtoken DB에 저장/조회하는 기능 추가 필요함

--- src/modules/jwtHandlers.ts
+++ src/modules/jwtHandlers.ts
@@ -1,7 +1,10 @@
 import jwt from 'jsonwebtoken';
+import userService from '../service/user';
 const { secretKey, options, refreshOptions } = require('../config/secretKey');
 const TOKEN_EXPIRED = -3;
 const TOKEN_INVALID = -2;
+
+//공부해서 any 파티 처리하기.. 오.. 애니파티.. 라임쩌는데..
 
 const sign = async (user: any) => {
   const payload = {
@@ -35,7 +38,42 @@ const verify = async (token: any) => {
   return decoded;
 };
 
+const refresh = async (refreshToken: any) => {
+  try {
+      const result:any= await jwt.verify(refreshToken, secretKey);
+      if (result.id === undefined) {
+          return TOKEN_INVALID;
+      }
+      const user:any = await userService.getUserByID(result.id);
+      if (refreshToken !== user.refreshToken) {
+          console.log('invalid refresh token');
+          return TOKEN_INVALID;
+      }
+      const payload = {
+        idx: user.idx,
+        id: user.id
+      };
+      const dto = {
+          token: jwt.sign(payload, secretKey, options)
+      };
+      return dto;
+  } catch (err: any) {
+      if (err.message === 'jwt expired') {
+          console.log('expired token');
+          return TOKEN_EXPIRED;
+      } else if (err.message === 'invalid token') {
+          console.log('invalid token');
+          console.log(TOKEN_INVALID);
+          return TOKEN_INVALID;
+      } else {
+          console.log("invalid token");
+          return TOKEN_INVALID;
+      }
+  }
+}
+
 export default {
   sign,
-  verify
+  verify,
+  refresh
 }
